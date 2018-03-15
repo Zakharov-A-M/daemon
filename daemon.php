42443559a406f0aec95d2c19f9990a50a03e549d
Alexander Zakharov <an.zakharov@andersenlab.com>
2018-03-15 22:11:48 +0300

    fix time sendig request method update

--- daemon.php
+++ daemon.php
@@ -5,16 +5,19 @@ include_once('curl.php');
 include_once('email.php');
 include_once('json.php');
 
-const HOUR = 3;
+const HOUR = 3600;
 
 try {
+    $timeStart = time();
     $response = new Curl();
     $crypt =  new Crypt();
     while (true) {
         $responseArray = $response->performRequest(['method' => 'get']);
         $output = $crypt->cryptXor($responseArray['message'], $responseArray['key']);
-        $response->performRequest(['method' => 'update', 'message' => $output]);
-        sleep(HOUR);
+        if ((time() - $timeStart) >= HOUR) {
+            $response->performRequest(['method' => 'update', 'message' => $output]);
+            $timeStart = time();
+        }
     }
 } catch (Exception $e) {
     Email::sendByEmail(['message' => $e->getMessage(), 'code' =>$e->getCode()]);
